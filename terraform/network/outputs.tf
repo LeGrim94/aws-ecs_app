e4aaa7bc4f92e49a75cfa7c20d3da316692650fb
Giuseppe Grimaldi <giuseppe.grimaldi@it.clara.net>
2024-03-04 16:26:40 +0100

    feat(network): created blank frontend modules and added outputs to network module

---
+++ terraform/network/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Output of private subnet addresses"
+  value       = module.vpc.private_subnets
+}
+
+output "database_subnets" {
+  description = "Output of database subnet addresses"
+  value       = module.vpc.database_subnets
+}
+
+output "database_subnet_group_name" {
+  description = "Name of the database subnet group"
+  value       = module.vpc.database_subnet_group_name
+}
+
+output "public_subnets" {
+  description = "Public subnets of the VPC"
+  value       = module.vpc.public_subnets
+}
+
+output "zone_id" {
+  description = "ID of the private zone created by Route53"
+  value       = aws_route53_zone.private.id
+}
