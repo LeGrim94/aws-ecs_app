2cfb579e881f7d40690750db44c8a87f600d7589
Giuseppe Grimaldi <giuseppe.grimaldi@it.clara.net>
2024-03-07 00:26:59 +0100

    chore: uncomment task efs policy

--- terraform/ecs/iam.tf
+++ terraform/ecs/iam.tf
@@ -46,16 +46,16 @@ module "ecs_task_execution_role" {
   number_of_custom_role_policy_arns = 1
 }
 
-# data "aws_iam_policy_document" "ecs_task_policy" {
-#   statement {
-#     sid = "AmazonECSTaskExecutionRolePolicy"
-#     actions = [
-#       "elasticfilesystem:ClientMount",
-#       "elasticfilesystem:ClientWrite",
-#     ]
-#     resources = [aws_efs_file_system.this.arn]
-#   }
-# }
+data "aws_iam_policy_document" "ecs_task_policy" {
+  statement {
+    sid = "AmazonECSTaskExecutionRolePolicy"
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientWrite",
+    ]
+    resources = [aws_efs_file_system.this.arn]
+  }
+}
 
 
 data "aws_iam_policy_document" "ecs_execute_policy" {
@@ -82,14 +82,14 @@ module "ecs_execute_task_policy" {
 }
 
 
-# module "ecs_task_policy" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-#   version = "3.16.0"
+module "ecs_task_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "3.16.0"
 
-#   name        = "wp-task-policy"
-#   description = "Amazon ECS Task Role Policy"
-#   policy      = data.aws_iam_policy_document.ecs_task_policy.json
-# }
+  name        = "wp-task-policy"
+  description = "Amazon ECS Task Role Policy"
+  policy      = data.aws_iam_policy_document.ecs_task_policy.json
+}
 
 module "ecs_task_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
@@ -104,9 +104,9 @@ module "ecs_task_role" {
   ]
 
   custom_role_policy_arns = [
-    #module.ecs_task_policy.arn,
+    module.ecs_task_policy.arn,
     module.ecs_execute_task_policy.arn
   ]
 
-  number_of_custom_role_policy_arns = 1
+  number_of_custom_role_policy_arns = 2
 }
